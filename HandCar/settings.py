96b29b0dff054644e35f5e0e53af9003c7400569
Yadu Krishna EH <85838175+yadhukrishnaofficial@users.noreply.github.com>
2025-05-24 14:08:42 +0530

    Update settings.py

--- HandCar/settings.py
+++ HandCar/settings.py
@@ -42,7 +42,9 @@ SECRET_KEY = 'django-insecure-g*6$8q350_^v7k=e-%ky4$&nn48ds8=&mvpqi5&)j=d_n5b(b!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','51.20.223.9','handcar.ae','admin.handcar.ae','api.handcar.ae']
+# ALLOWED_HOSTS = ['localhost','51.20.223.9','handcar.ae','admin.handcar.ae','api.handcar.ae']
+ALLOWED_HOSTS = ['*']
+
 
 
 
