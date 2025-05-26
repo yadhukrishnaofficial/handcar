974f6cbb1e8130a4955e114c1626535570c0bb0a
Yadu Krishna EH <85838175+yadhukrishnaofficial@users.noreply.github.com>
2025-05-26 16:16:19 +0530

    Update settings.py

--- HandCar/settings.py
+++ HandCar/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g*6$8q350_^v7k=e-%ky4$&nn48ds8=&mvpqi5&)j=d_n5b(b!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
